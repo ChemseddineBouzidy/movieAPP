6ba09208897f7efcfe26d9df27007afc237e718a
ChemseddineBouzidy <chamseddinexd@gmail.com>
2025-04-06 06:35:01 +0000

    tabs

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,12 @@
 import { Stack } from "expo-router";
 import "./globals.css";
+import { Header } from "react-native/Libraries/NewAppScreen";
 
 export default function RootLayout() {
-  return <Stack />;
+  return <Stack >
+    <Stack.Screen name="(tabs)" options={{HeaderShown: false}} />
+    <Stack.Screen name="(movies/[id]" options={{HeaderShown: false}} />
+
+  </Stack>
+ 
 }
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,28 @@ module.exports = {
   content: ["./app/**/*.{js,jsx,ts,tsx}"],
   presets: [require("nativewind/preset")],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: "#030014",
+        secondary: "#151312",
+        light:{
+          100: "#D6C6FF",
+          200: "#A8B5DB",
+          300: "#9CA4AB",
+        },
+        dark:{
+          100: "#221f3d",
+          200: "#0f0d23",
+        },
+        accent: "#AB8BFF",
+        neutral: "#374151",
+        "base-100": "#FFFFFF",
+        info: "#3ABFF8",
+        success: "#36D399",
+        warning: "#FBBD23",
+        error: "#F87272",
+      },
+    },
   },
   plugins: [],
 }
